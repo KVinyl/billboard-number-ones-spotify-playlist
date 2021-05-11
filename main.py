59fd26cf3257080782da0db2ff1a9df2fe5a6ce4
Kevin Lie <kevinyilie@gmail.com>
2021-05-11 06:39:14 -0400

    expand data extraction to every year in Billboard Hot 100 history

--- main.py
+++ main.py
@@ -4,25 +4,40 @@ from bs4 import BeautifulSoup
 from collections import namedtuple
 
 Song = namedtuple("Song", ["artist", "title"])
-song_list = []
 
-url_2010s = "https://en.wikipedia.org/wiki/List_of_Billboard_Hot_100_number-one_singles_of_the_2010s"
 
-response = requests.get(url_2010s)
-response.raise_for_status()
-webpage = response.text
+def get_songs(url):
+    song_list = []
 
-soup = BeautifulSoup(webpage, "html.parser")
+    response = requests.get(url)
+    response.raise_for_status()
+    webpage = response.text
 
-table = soup.find(class_="wikitable")
-rows = table.find_all("tr")
+    soup = BeautifulSoup(webpage, "html.parser")
+    table = soup.find(class_="wikitable")
+    rows = table.find_all("tr")
 
-for row in rows[1:]:
-    cells = row.find_all("td")
-    try:
-        song_list.append(Song(artist=cells[2].text.strip(), title=cells[3].text.split('"')[1]))
-    except IndexError:
-        pass
+    for row in rows[1:]:
+        cells = row.find_all("td")
+        try:
+            song_list.append(Song(artist=cells[2].text.strip(), title=cells[3].text.split('"')[1]))
+        except IndexError:
+            pass
 
-print(song_list)
-print(len(song_list))
+    return song_list
+
+
+wiki_urls = [
+    "https://en.wikipedia.org/wiki/List_of_Billboard_Hot_100_number-one_singles_from_1958_to_1969",
+    "https://en.wikipedia.org/wiki/List_of_Billboard_Hot_100_number-one_singles_of_the_1970s",
+    "https://en.wikipedia.org/wiki/List_of_Billboard_Hot_100_number-one_singles_of_the_1980s",
+    "https://en.wikipedia.org/wiki/List_of_Billboard_Hot_100_number-one_singles_of_the_1990s",
+    "https://en.wikipedia.org/wiki/List_of_Billboard_Hot_100_number-one_singles_of_the_2000s",
+    "https://en.wikipedia.org/wiki/List_of_Billboard_Hot_100_number-one_singles_of_the_2010s",
+    "https://en.wikipedia.org/wiki/List_of_Billboard_Hot_100_number-one_singles_of_the_2020s"
+]
+
+number_ones = [song for url in wiki_urls for song in get_songs(url)]
+
+print(number_ones)
+print(len(number_ones))
